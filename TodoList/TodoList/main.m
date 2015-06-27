366d0502eebf6ad3669a673e651fbf9fbb347a86
Eric Sze <you@example.com>
2015-06-27 15:16:14 -0400

    new edits

--- TodoList/TodoList/main.m
+++ TodoList/TodoList/main.m
@@ -8,10 +8,48 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Manager: NSObject
+
+@property (nonatomic) NSMutableArray *lists;
+
+//-(void)arr:(NSMutableArray)
+//-(void)print;
+
+@end
+
+@interface list: NSObject
+@end
+
+@interface item: NSObject
+@end
+
+@implementation Manager
+
+
+
+- (void)print {
+    NSLog(@"\n1)Add item to list\n2)Remove item from list\n3)Create new list");
+}
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSMutableArray *arr = [[NSMutableArray alloc] init];
+        Manager *m = [[Manager alloc] init];
+        [m print];
+        if (print == buf) {
+            NSLog(@"%s", buf);
+        }
+//        NSMutableArray *arr = [[NSMutableArray alloc] initWithCapacity:3];
+//        for (int i = 0; i < 3; i++) {
+            char buf[100];
+            scanf("%s", buf);
+//            NSString *str = [NSString stringWithCString:buf encoding:NSASCIIStringEncoding];
+//            [arr addObject:str];
+//        }
+    
     }
     return 0;
 }
